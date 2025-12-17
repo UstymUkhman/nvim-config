8e199050a2e45047c6788e1ae9c3c2ee9e0cd40c
Ustym Ukhman <ustym.ukhman@gmail.com>
2025-12-17 14:33:13 +0200

    Add Configs

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<C-j>", "^", { desc = "Go to the start of the line", remap = true })
+map("n", "<C-k>", "$", { desc = "Go to the end of the line", remap = true })
+
+map("i", "jj", "<esc>", { desc = "Switch to normal mode", silent = true })
+map("i", "jk", "<esc>", { desc = "Switch to normal mode", silent = true })
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/ustym/.config/omarchy/current/theme/neovim.lua
