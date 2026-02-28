5410d8a3dfc8f3549b242085d725a6aa29720426
Ustym Ukhman <ustym.ukhman@gmail.com>
2026-02-28 19:38:35 +0200

    Omarchy Update 3.4.0

--- cmd.sh
+++ cmd.sh
@@ -3,3 +3,9 @@ sudo snapper -c root list
 
 # Delete snapshot with ID 5:
 sudo snapper -c root delete 5
+
+# Handle systemd services:
+sudo systemctl poweroff
+
+# Omarchy system menu script location:
+# /home/ustym/.local/share/omarchy/bin/omarchy-menu
